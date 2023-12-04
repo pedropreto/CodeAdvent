d278ad6744a83245e8c5c9d9a4b863dcb61f840a
Preto <pedroppreto@gmail.com>
2023-12-04 09:27:48 +0000

    Day 4 part 2 2023

--- 2023/day4.py
+++ 2023/day4.py
@@ -32,6 +32,29 @@ def part1():
     return final_number
 
 
-result = part1()
+def part2():
+    card_points = []  # useless list because they didn't ask for points
+    cards_reps = [1] * len(lines)  # list of repetitions of the cards
+    for line in lines:
+        matching_numbers = []
+        game_number_part, cards = line.split(':')
+        game_number = int(re.findall(r'\d+', game_number_part)[0])
+        winning_card, my_card = cards.split('|')
+        winning_numbers = re.findall(r'\d+', winning_card)
+        my_numbers = re.findall(r'\d+', my_card)
+        for number in my_numbers:
+            if number in winning_numbers:
+                matching_numbers.append(number)
+
+        for i in range(game_number, game_number - 1 + len(matching_numbers) + 1):
+            cards_reps[i] += 1 * cards_reps[game_number - 1]
+
+        single_card_points = 2 ** (len(matching_numbers) - 1) if len(matching_numbers) > 0 else 0
+        card_points.append(single_card_points)
+
+    return sum(cards_reps)
+
+
+result = part2()
 print(result)
 
