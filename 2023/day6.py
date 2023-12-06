97c461c024270fefadac2a8ab0cbe928f20c6848
Preto <pedroppreto@gmail.com>
2023-12-06 08:40:30 +0000

    Day 6 2023

---
+++ 2023/day6.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import os
+import re
+import numpy as np
+
+file = 'day' + re.findall(r'\d+', os.path.basename(__file__))[0] + '.txt'
+file_path = os.path.join("inputs", file)
+
+# opens the file
+with open(file_path) as f:
+    lines = [line.rstrip() for line in f]  # takes the \n
+
+
+def part1():
+
+    times = re.findall(r'\d+', lines[0].split(':')[1])
+    record_distances = re.findall(r'\d+', lines[1].split(':')[1])
+
+    final_number = 1
+    beating_ways = [0] * len(times)
+    for idx, time in enumerate(times):
+        for i in range(1, int(time)):
+            distance = (int(time) - i) * i
+
+            beating_ways[idx] += 1 if distance > int(record_distances[idx]) else 0
+
+    for way in beating_ways:
+        final_number *= way
+
+    return final_number
+
+
+def part2():
+
+    times = re.findall(r'\d+', lines[0].split(':')[1])
+    record_distances = re.findall(r'\d+', lines[1].split(':')[1])
+
+    time, record_distance = '', ''
+    for x in times:
+        time = time + x
+
+    for y in record_distances:
+        record_distance = record_distance + y
+
+    beating_ways = 0
+    for i in range(1, int(time)):
+        distance = (int(time) - i) * i
+        beating_ways += 1 if distance > int(record_distance) else 0
+
+        if i > int(time) / 2 and distance < int(record_distance):
+            break
+
+    return beating_ways
+
+
+result = part2()
+print(result)
