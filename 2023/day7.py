176ba73d428bd65d892e8d7d3f67e8798048540e
Preto <pedroppreto@gmail.com>
2023-12-07 22:59:13 +0000

    Day 7 Part 1 2023

---
+++ 2023/day7.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import math
+import time
+import os
+import re
+import numpy as np
+
+start = time.time() * 1000
+
+file = 'day' + re.findall(r'\d+', os.path.basename(__file__))[0] + '.txt'
+file_path = os.path.join("inputs", file)
+
+# opens the file
+with open(file_path) as f:
+    lines = [line.rstrip() for line in f]  # takes the \n
+
+
+def part1():
+    hands, bids = [], []
+    for line in lines:
+        hand, bid = line.split(' ')
+        hands.append(hand), bids.append(bid)
+
+    ranks = [0] * len(hands)
+    for i, hand1 in enumerate(hands):
+        won = 0
+        for j, hand2 in enumerate(hands):
+            if i != j:
+                result = compare_hands([hand1, hand2])
+                if result:
+                    won += 1
+
+        ranks[i] = won + 1
+
+        product = [int(x) * int(y) for x, y in zip(ranks, bids)]
+
+    return sum(product)
+
+
+def compare_hands(hand_list):
+    first_hand, second_hand = hand_list
+    card_value = {"1": 1, "2": 2, "3": 3, "4": 4, "5":5, "6": 6, "7": 7, "8": 8, "9": 9,
+                  "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+    # count cards in each hand
+    count_dict_fh, count_dict_sh = {}, {}
+    fh_unique, sh_unique = "".join(set(first_hand)), "".join(set(second_hand))
+    for idx, card in enumerate(fh_unique):
+        fh_counter = first_hand.count(card)
+        count_dict_fh[card] = fh_counter
+
+    for idx, card in enumerate(sh_unique):
+        sh_counter = second_hand.count(card)
+        count_dict_sh[card] = sh_counter
+
+    hands_counter = [count_dict_fh, count_dict_sh]
+    points = [0, 0]
+
+    for idx, dict in enumerate(hands_counter):
+        for key, value in dict.items():
+            points[idx] += (value - 1) ** 2
+
+    if points[0] > points[1]:
+        return True
+    elif points[1] > points[0]:
+        return False
+    else:
+        for card1, card2 in zip(first_hand, second_hand):
+            if card_value[card1] > card_value[card2]:
+                return True
+            if card_value[card2] > card_value[card1]:
+                return False
+
+
+result = part1()
+print(result)
+
+end = time.time() * 1000
+
+print(f'This took {(end-start)} miliseconds')
