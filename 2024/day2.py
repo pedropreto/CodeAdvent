a9fe07ec64e5a5da5e72ad720c866f4a510105dd
Pedro <pedroppreto@gmail.com>
2024-12-08 23:22:05 +0000

    Day 2 2024 Part 2

--- 2024/day2.py
+++ 2024/day2.py
@@ -13,66 +13,46 @@ with open(file_path) as f:
 
 
 def part1():
+    pattern = r"\d+"
     safe_reports = 0
-    dif_total_lst = list()
     for line in lines:
-        dif_lst = list()
-        line = line.split(' ')
-        print(line)
+        levels = re.findall(pattern, line)
 
-        for i in range(0, len(line)-1):
-            dif_lst.append(int(line[i]) - int(line[i+1]))
-        print(dif_lst)
-
-        if check_safe(dif_lst):
+        if check_safe(levels):
             safe_reports += 1
-        dif_total_lst.append(dif_lst)
-
 
     return safe_reports
 
 
 def part2():
+    pattern = r"\d+"
     safe_reports = 0
     for line in lines:
-        line = line.split(' ')
-        print(line)
+        levels = re.findall(pattern, line)
 
-        dif_lst = calc_dif(line)
-
-        safe, unsafe_idx = check_safe_v2(dif_lst)
-        if safe:
-            # if safe, count immediately
+        if check_safe(levels):
             safe_reports += 1
-            print('Safe')
         else:
-            # if unsafe, try to remove one element
-            # if unsafe idx is on the second part of the list, add 1
-            if unsafe_idx >= len(line) / 2:
-                unsafe_idx += 1
-
-            # check what is the unsafe idx and remove it from the original list
-            line.pop(unsafe_idx)
-            new_dif_lst = calc_dif(line)
-            # check again if it's safe
-            safe, unsafe_idx = check_safe_v2(new_dif_lst)
-            if safe:
-                safe_reports += 1
-                print('Turned safe')
+            # brute force remove 1 by 1
+            for i in range(0, len(levels)):
+                levels_copy = list(levels)
+                del levels_copy[i]
+                if check_safe(levels_copy):
+                    safe_reports += 1
+                    break
 
     return safe_reports
 
 
-def calc_dif(lst):
-    dif_lst = list()
-    for i in range(0, len(lst) - 1):
-        dif_lst.append(int(lst[i]) - int(lst[i + 1]))
-    return dif_lst
 
-def check_safe(lst):
+def check_safe(levels):
+    dif_lst = []
+    for i in range(0, len(levels) - 1):
+        dif_lst.append(int(levels[i]) - int(levels[i + 1]))
+
     positive_count = negative_count = zeros = overslope = 0
 
-    for num in lst:
+    for num in dif_lst:
         positive_count += num > 0
         negative_count += num < 0
         zeros += num == 0
@@ -82,34 +62,6 @@ def check_safe(lst):
         return False
 
     return True
-
-def check_safe_v2(lst):
-    positive_count = negative_count = zeros = overslope = 0
-
-    for num in lst:
-        positive_count += num > 0
-        negative_count += num < 0
-        zeros += num == 0
-        overslope += abs(num) > 3
-
-    if not ((positive_count > 0 and negative_count > 0) or zeros > 0 or overslope > 0):
-        return True, -1
-
-    if zeros > 0:
-        unsafe_idx = [i for i, num in enumerate(lst) if num == 0]
-    elif overslope > 0:
-        unsafe_idx = [i for i, num in enumerate(lst) if abs(num) > 3]
-    elif positive_count > negative_count:
-        unsafe_idx = [i for i, num in enumerate(lst) if num < 0]
-    else:
-        unsafe_idx = [i for i, num in enumerate(lst) if num > 0]
-
-    return False, unsafe_idx[0]
-
-
-
-
-
 
 result = part2()
 print(result)
