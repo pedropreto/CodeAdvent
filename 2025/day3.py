c298d20fcfdc31a46e2d5f90d8f08a373302c195
Preto <pedroppreto@gmail.com>
2025-12-06 00:19:26 +0000

    Day 3 part 2 2025

--- 2025/day3.py
+++ 2025/day3.py
@@ -37,5 +37,35 @@ def part1():
 
     return sum(joltages)
 
-result = part1()
+
+def part2():
+    joltages = []
+    n_total_digits = len(lines[0])
+    n_batteries = 12
+    max_excluded = n_total_digits - n_batteries
+    for bank in lines:
+        excluded = 0
+        final_bank = ''
+        # Check the largest in the section and exclude the rest
+        # continue checking for each section after the last digit chosen
+        # when no more can be excluded, end
+        while excluded < max_excluded:
+            digit, digit_idx = check_largest_digit(bank[:max_excluded-excluded+1])
+            final_bank += str(digit)
+
+            excluded += digit_idx
+            bank = bank[digit_idx + 1:]
+            if len(final_bank) >= n_batteries:
+                bank = ''
+                break
+
+        final_bank += bank
+        joltages.append(int(final_bank))
+
+    return sum(joltages)
+
+
+
+
+result = part2()
 print(result)
